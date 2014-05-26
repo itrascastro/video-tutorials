d6465efe0fcef15ee057e5f512c8ce9c7f4be645
itrascastro <i.trascastro@gmail.com>
2014-05-26 10:57:49 +0200

    no-mvc

---
+++ no-mvc/doSubtract.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>doSubtract</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<?php echo $_POST['op1'] - $_POST['op2']; ?>
+
+<p>Footer</p>
+
+</body>
+</html>
